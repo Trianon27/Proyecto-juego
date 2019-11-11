5f3efa4089cd508a47953e5418ff0c7bd63eac03
slondonoq <>
2019-11-10 21:03:42 -0500

    Borrador clase armor

---
+++ ProyectoJuego/src/modelo/Armor.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author User
+ */
+public class Armor {
+    private String name;
+    private int baseDefense;
+    private String wholeSetEffect;
+    private double wholeSetEffectBonus;
+
+    public Armor() {
+        name = "???";
+        baseDefense = 0;
+        wholeSetEffect = "None";
+        wholeSetEffectBonus = 0;
+    }
+
+    public Armor(String name, int baseDefense, String wholeSetEffect, double wholeSetEffectBonus) {
+        this.name = name;
+        this.baseDefense = baseDefense;
+        this.wholeSetEffect = wholeSetEffect;
+        this.wholeSetEffectBonus = wholeSetEffectBonus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBaseDefense() {
+        return baseDefense;
+    }
+
+    public void setBaseDefense(int baseDefense) {
+        this.baseDefense = baseDefense;
+    }
+
+    public String getWholeSetEffect() {
+        return wholeSetEffect;
+    }
+
+    public void setWholeSetEffect(String wholeSetEffect) {
+        this.wholeSetEffect = wholeSetEffect;
+    }
+
+    public double getWholeSetEffectBonus() {
+        return wholeSetEffectBonus;
+    }
+
+    public void setWholeSetEffectBonus(double wholeSetEffectBonus) {
+        this.wholeSetEffectBonus = wholeSetEffectBonus;
+    }
+    
+}
