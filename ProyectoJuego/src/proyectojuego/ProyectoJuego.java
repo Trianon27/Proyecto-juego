11948611a6ca83ea7272cee131af46944a5d3472
Leand <Leand@DESKTOP-0IVCKDM>
2020-02-03 08:42:03 -0500

    Actualización clase main  (ahora clase Ambiente)
    En la clase ambiente se añadieron las mecanicas de mov del hero principal
    al ejecutar muestra una imagen que se puede mover en un jpanel

--- ProyectoJuego/src/proyectojuego/ProyectoJuego.java
+++ ProyectoJuego/src/proyectojuego/ProyectoJuego.java
@@ -11,11 +11,6 @@ package proyectojuego;
  */
 public class ProyectoJuego {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   
     
 }
