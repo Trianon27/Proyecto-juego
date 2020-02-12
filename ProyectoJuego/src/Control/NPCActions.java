52c5d4b8eb090c963ccd36e79108b583e90f932b
srodr <srodr@LAPTOP-IMJ8UETG>
2020-02-11 22:43:30 -0500

    Checking error

--- ProyectoJuego/src/Control/NPCActions.java
+++ ProyectoJuego/src/Control/NPCActions.java
@@ -4,6 +4,7 @@ package Control;
 import modelo.Hero;
 import modelo.NPC;
 import Control.ItemManagement;
+import modelo.Item;
 
 
 public class NPCActions {
@@ -26,7 +27,7 @@ public class NPCActions {
 
     
     
-    public void Npcactions (NPC Nperson, NPC npc){
+    public void NpcIdeas (NPC Nperson, NPC npc){
          //first we check if the NPC is enemy, allie or neutral and also if the hero or npc is alive
          
          if (hero.getState()== "alive" || npc.getState()=="alive"){
@@ -47,7 +48,7 @@ public class NPCActions {
                                 }else{}
                                 break;
                     // Now we repeat the same procedure with npc´s
-                          case "sllie":
+                          case "allie":
                              Nperson.setAction("attack");
                              npc.setMaxHealth(npc.getCurrentHealth()-Nperson.getMeleeDamage());
                                 //Now we check the state of the hero, if his current health is 0 or less than 0, he will die
@@ -88,9 +89,7 @@ public class NPCActions {
                         }
                         break;
                         
-                 case "neutral":
-                     //The only interactions that can has the neutral npc is with the hero
-                     
+              
                            
            }
         //If the npc or hero are dead, the interaction dont ocurred          
@@ -99,6 +98,37 @@ public class NPCActions {
          
     }
     
+    public void HeroToNpc (Item Herow, NPC nPc){
+        //First we specfied what kind of attack use the hero
+        switch (Herow.getType() ){
+        
+            case "spell": 
+                //Now we look how many damage the hero cause. 
+                int HeroMagicDamage=Herow.getMagicDamage(); 
+                //Now we take the defense of NPC
+                int npcMagicDefense= nPc.getMagicDefense(); 
+                //now we interact, but we have to do the next equiation, the total damage will be split by defense
+                int totalMagicDamage= HeroMagicDamage/npcMagicDefense;
+                nPc.setCurrentHealth(nPc.getCurrentHealth()-totalMagicDamage);
+                
+                break; 
+                
+            case "Principal_Weapon":
+                int HeroDamage=Herow.getDamage(); 
+                
+                int npcDefense= nPc.getDefense(); 
+                
+                int totalDamage= HeroDamage/npcDefense;
+                nPc.setCurrentHealth(nPc.getCurrentHealth()-totalDamage);
+                
+                break; 
+        
+        }
+    
+    
+    
+    
+    }
 
     
  }
